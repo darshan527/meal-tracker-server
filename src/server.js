9044a9c13a7a9d5fe5b1106e1792909f2afa6f8d
darshan527 <darshan.hosamani6@gmail.com>
2021-06-27 23:16:19 +0530

    Added Routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import express from 'express';
-
+import { routes } from './routes';
 const app = express();
+
+
+
+routes.forEach(route => {
+    app[route.method](route.path, route.handler);
+});
 
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
