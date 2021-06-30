0940d8d5a457fd55c5a03674916509832cbfc10b
darshan527 <darshan.hosamani6@gmail.com>
2021-06-30 23:47:00 +0530

    Fetches Recipes from Database

---
+++ src/db/getRecipes.js
@@ -0,0 +1,8 @@
+import { db } from "./db";
+
+export const getRecipes = async () => {
+  const connection = db.getConnection();
+  const recipes = await connection.collection("recipes").find({}).toArray();
+
+  return recipes;
+};
