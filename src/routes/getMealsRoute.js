74c63de625aed652bf85efd0516389bd9eb15196
darshan527 <darshan.hosamani6@gmail.com>
2021-06-30 23:44:57 +0530

    updated import

--- src/routes/getMealsRoute.js
+++ src/routes/getMealsRoute.js
@@ -1,10 +1,10 @@
-import { getMeals } from '../db'
+import { getPopulatedMeals } from "../db";
 
 export const getMealsRoute = {
-    method: 'get',
-    path: '/meals',
-    handler: async (req, res) => {
-        const meals = await getMeals()
-        res.status(200).json(meals)
-    },
-}
+  method: "get",
+  path: "/meals",
+  handler: async (req, res) => {
+    const meals = await getPopulatedMeals();
+    res.status(200).json(meals);
+  },
+};
