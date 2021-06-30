7c8540d488736fdfcae9ddc9417f1caf9e838713
darshan527 <darshan.hosamani6@gmail.com>
2021-06-30 23:45:51 +0530

    formatted code

--- src/db/getMeals.js
+++ src/db/getMeals.js
@@ -1,8 +1,7 @@
 import { db } from "./db";
 
 export const getMeals = async () => {
-    const connection = await db.getConnection()
-    const meals = await connection.collection('meals')
-        .find({}).toArray();
-    return meals
-}
+  const connection = await db.getConnection();
+  const meals = await connection.collection("meals").find({}).toArray();
+  return meals;
+};
