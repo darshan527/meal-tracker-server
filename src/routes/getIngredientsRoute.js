eab9776f927007e3b1be44fa4e96258e7a975537
darshan527 <darshan.hosamani6@gmail.com>
2021-07-06 23:18:37 +0530

    wip

--- src/routes/getIngredientsRoute.js
+++ src/routes/getIngredientsRoute.js
@@ -1,7 +1,10 @@
-export const getIngredientsRoute = {
-    method: 'get',
-    path: '/ingredients',
-    handler: async (req, res) => {
+import { getIngredients } from "../db";
 
-    },
-}
+export const getIngredientsRoute = {
+  method: "get",
+  path: "/ingredients",
+  handler: async (req, res) => {
+    const ingredients = await getIngredientsRoute();
+    res.status(200).json(ingredients);
+  },
+};
