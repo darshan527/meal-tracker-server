c85c322968b74262062940a1aaefe475dccf1f6f
darshan527 <darshan.hosamani6@gmail.com>
2021-07-09 00:40:59 +0530

    fixed route issue

--- src/routes/getIngredientsRoute.js
+++ src/routes/getIngredientsRoute.js
@@ -4,7 +4,7 @@ export const getIngredientsRoute = {
   method: "get",
   path: "/ingredients",
   handler: async (req, res) => {
-    const ingredients = await getIngredientsRoute();
+    const ingredients = await getIngredients();
     res.status(200).json(ingredients);
   },
 };
