161fadd0496e2113526286be0e9b62d29f2a3c1a
darshan527 <darshan.hosamani6@gmail.com>
2021-07-09 22:22:56 +0530

    wip

--- src/routes/deleteMealRoute.js
+++ src/routes/deleteMealRoute.js
@@ -1,7 +1,12 @@
+import { getPopulatedMeals } from "../db";
+
 export const deleteMealRoute = {
     method: 'delete',
     path: '/meals/:id',
     handler: async (req, res) => {
-
+        const { id } = req.params;
+        await deleteMealRoute(id);
+        const updatedMeals = await getPopulatedMeals();
+        res.status(200).json(updatedMeals);
     },
 }
