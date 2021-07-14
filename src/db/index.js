2e3f214228ab95a91922cc8c39251fbc75c15e3a
darshan527 <darshan.hosamani6@gmail.com>
2021-07-14 13:15:57 +0530

    Added deleteMeal feature

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,4 @@
 export { db } from "./db";
 export { getPopulatedMeals } from "./getPopulatedMeals";
 export { getIngredients } from "./getIngredients";
+export { deleteMeal } from "./deleteMeal";
--- src/routes/deleteMealRoute.js
+++ src/routes/deleteMealRoute.js
@@ -1,11 +1,11 @@
-import { getPopulatedMeals } from "../db";
+import { deleteMeal, getPopulatedMeals } from "../db";
 
 export const deleteMealRoute = {
     method: 'delete',
     path: '/meals/:id',
     handler: async (req, res) => {
         const { id } = req.params;
-        await deleteMealRoute(id);
+        await deleteMeal(id);
         const updatedMeals = await getPopulatedMeals();
         res.status(200).json(updatedMeals);
     },
