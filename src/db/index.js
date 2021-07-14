2c9f0e991135ff008f3ee2c209bee1969fd44ed8
darshan527 <darshan.hosamani6@gmail.com>
2021-07-14 23:36:31 +0530

    Added deleteIngredient

--- src/db/index.js
+++ src/db/index.js
@@ -2,3 +2,4 @@ export { db } from "./db";
 export { getPopulatedMeals } from "./getPopulatedMeals";
 export { getIngredients } from "./getIngredients";
 export { deleteMeal } from "./deleteMeal";
+export { deleteIngredient } from "./deleteIngredient";
